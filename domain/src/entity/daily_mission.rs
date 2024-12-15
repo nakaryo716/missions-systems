033883c3ae99386a2b8152ee84cd31c42e5f0554
ryonakaya <nakayaryo0716@gmail.com>
2024-12-15 12:23:39 +0900

    [fix]add mission_id filed

--- domain/src/entity/daily_mission.rs
+++ domain/src/entity/daily_mission.rs
@@ -1,8 +1,9 @@
-use super::user_id::UserId;
+use super::{daily_mission_id::DailyMissionId, user_id::UserId};
 
 #[derive(Debug, Clone)]
 pub struct DailyMission {
     pub user_id: UserId,
+    pub mission_id: DailyMissionId,
     pub title: String,
     pub description: Option<String>,
     pub is_complete: bool,
