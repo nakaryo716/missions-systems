5b054b7a47d5bd7cf37b81ffb6035c26d81f2474
ryonakaya <nakayaryo0716@gmail.com>
2024-12-21 16:44:52 +0900

    [fix]add derivce PartialEq, Eq

--- domain/src/entity/user_id.rs
+++ domain/src/entity/user_id.rs
@@ -1,4 +1,4 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct UserId(pub String);
