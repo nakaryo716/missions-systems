6d5eb8fbe7f1fc6caaf1902b8987b54aa774acb4
ryonakaya <nakayaryo0716@gmail.com>
2024-12-21 18:51:33 +0900

    [add]add daily_mission_repository_impl module

--- infrastructure/src/repository/mod.rs
+++ infrastructure/src/repository/mod.rs
@@ -1,6 +1,7 @@
 use domain::repository::repository_error::RepositoryError;
 use sqlx::Error;
 
+pub mod daily_mission_repository_impl;
 pub mod user_repository_impl;
 
 fn to_repo_err(e: sqlx::error::Error) -> RepositoryError {
