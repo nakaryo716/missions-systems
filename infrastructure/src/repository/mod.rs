0ef3c9baa1cf45a5b7442a16c818a3546476504d
ryonakaya <nakayaryo0716@gmail.com>
2024-12-21 16:46:34 +0900

    [add]add user_repository_impl module

---
+++ infrastructure/src/repository/mod.rs
@@ -0,0 +1,12 @@
+use domain::repository::repository_error::RepositoryError;
+use sqlx::Error;
+
+pub mod user_repository_impl;
+
+fn to_repo_err(e: sqlx::error::Error) -> RepositoryError {
+    match e {
+        Error::RowNotFound => RepositoryError::NotFound,
+        Error::TypeNotFound { type_name } => RepositoryError::InvalidData(type_name),
+        e => RepositoryError::DatabaseError(e.to_string()),
+    }
+}
