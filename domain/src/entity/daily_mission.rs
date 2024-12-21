f29992336a3bc39de07fda615227dc511c138bbe
ryonakaya <nakayaryo0716@gmail.com>
2024-12-21 22:21:42 +0900

    [add]impl FromRow trait

--- domain/src/entity/daily_mission.rs
+++ domain/src/entity/daily_mission.rs
@@ -1,3 +1,5 @@
+use sqlx::{mysql::MySqlRow, FromRow, Row};
+
 use super::{daily_mission_id::DailyMissionId, user_id::UserId};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -7,4 +9,16 @@ pub struct DailyMission {
     pub title: String,
     pub description: Option<String>,
     pub is_complete: bool,
+}
+
+impl FromRow<'_, MySqlRow> for DailyMission {
+    fn from_row(row: &'_ MySqlRow) -> Result<Self, sqlx::Error> {
+        Ok(Self {
+            user_id: UserId(row.try_get("user_id")?),
+            mission_id: DailyMissionId(row.try_get("mission_id")?),
+            title: row.try_get("title")?,
+            description: row.try_get("descriptions")?,
+            is_complete: row.try_get("is_complete")?,
+        })
+    }
 }
