0ade11911c260501093ff5910636d4ae15dc1d42
ryonakaya <nakayaryo0716@gmail.com>
2024-12-25 01:41:34 +0900

    [add]add cors layer settings

--- app_server/src/router.rs
+++ app_server/src/router.rs
@@ -2,11 +2,19 @@ use axum::{
     routing::{get, post, put},
     Router,
 };
+use http::{
+    header::{
+        ACCESS_CONTROL_ALLOW_CREDENTIALS, ACCESS_CONTROL_ALLOW_METHODS,
+        ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE,
+    },
+    HeaderValue, Method,
+};
 use sqlx::MySqlPool;
+use tower_http::cors::CorsLayer;
 
 use crate::handlers::{auth, daily_mission, exp, user};
 
-pub fn app(pool: MySqlPool) -> Router {
+pub fn app(pool: MySqlPool, allow_origin: &str) -> Router {
     Router::new()
         .route(
             "/user",
@@ -29,4 +37,23 @@ pub fn app(pool: MySqlPool) -> Router {
         .route("/daily/set/:id", put(daily_mission::set_complete))
         .route("/exp", get(exp::find).put(exp::add))
         .with_state(pool)
+        .layer(
+            CorsLayer::new()
+                .allow_origin(allow_origin.parse::<HeaderValue>().unwrap())
+                .allow_credentials(true)
+                .allow_methods([
+                    Method::OPTIONS,
+                    Method::POST,
+                    Method::GET,
+                    Method::PUT,
+                    Method::DELETE,
+                ])
+                .allow_headers([
+                    CONTENT_TYPE,
+                    ACCESS_CONTROL_ALLOW_ORIGIN,
+                    ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                    ACCESS_CONTROL_ALLOW_METHODS,
+                ])
+                .max_age(std::time::Duration::from_secs(3600)),
+        )
 }
