708613c941468e56c2375b59766421f8cb475735
ryonakaya <nakayaryo0716@gmail.com>
2025-01-03 12:03:22 +0900

    [add]derived Validate

--- domain/src/entity/daily_mission_input.rs
+++ domain/src/entity/daily_mission_input.rs
@@ -1,7 +1,10 @@
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Validate)]
 pub struct DailyMissionInput {
+    #[validate(length(min = 1, max = 20))]
     pub title: String,
+    #[validate(length(max = 100))]
     pub description: Option<String>,
 }
--- domain/src/entity/user_input.rs
+++ domain/src/entity/user_input.rs
@@ -1,9 +1,13 @@
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Validate)]
 #[serde(rename_all = "camelCase")]
 pub struct UserInput {
+    #[validate(length(min = 1, max = 10))]
     pub user_name: String,
+    #[validate(email)]
     pub email: String,
+    #[validate(length(min = 8, max = 15))]
     pub password: String,
 }
