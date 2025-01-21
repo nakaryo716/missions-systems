239cb54e6945f400acc600c1ae23c18b598b1481
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-01-21 23:43:31 +0900

    [fix]Change to get token from cookie to Authorization header

--- app_server/src/types/token_warper.rs
+++ app_server/src/types/token_warper.rs
@@ -1,6 +1,6 @@
-use axum::extract::FromRequestParts;
-use axum_extra::extract::CookieJar;
+use axum::{body::Bytes, extract::FromRequestParts};
 use domain::entity::token::Token;
+use http::header::AUTHORIZATION;
 
 use crate::error::AuthError;
 
@@ -15,7 +15,7 @@ where
     type Rejection = crate::error::AuthError;
     fn from_request_parts<'life0, 'life1, 'async_trait>(
         parts: &'life0 mut axum::http::request::Parts,
-        state: &'life1 S,
+        _state: &'life1 S,
     ) -> ::core::pin::Pin<
         Box<
             dyn ::core::future::Future<Output = Result<Self, Self::Rejection>>
@@ -29,14 +29,21 @@ where
         Self: 'async_trait,
     {
         Box::pin(async move {
-            let jar = CookieJar::from_request_parts(parts, state)
-                .await
-                .map_err(|_| AuthError::InvalidToken)?;
-            match jar.get("token") {
-                Some(cookie) => {
-                    let v = cookie.value();
-                    let token = TokenWrap(Token(v.to_string()));
-                    Ok(token)
+            // AUTHORIZATIONヘッダから値を取得
+            match parts
+                .headers
+                .get(AUTHORIZATION)
+                .and_then(|v| v.to_str().ok())
+            {
+                Some(val) => {
+                    // AUTHORIZATIONヘッダの値は"Bearer <TOKEN>"の形式でBearer+空白の部分はいらない
+                    // スライスをとってTOKEN部分だけを抽出する
+                    let header_val_bytes = Bytes::from(val.to_owned());
+                    let header_slice = header_val_bytes.slice(7..);
+                    // String型に変換
+                    let token = String::from_utf8(header_slice.to_vec())
+                        .map_err(|_| AuthError::InvalidToken)?;
+                    Ok(TokenWrap(Token(token)))
                 }
                 None => Err(AuthError::InvalidToken),
             }
