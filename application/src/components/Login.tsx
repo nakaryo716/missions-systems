56b8d8c9368660ec306e7226d37d1035ea321565
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-01-30 22:08:25 +0900

    [add]Added login feature

--- application/src/components/Login.tsx
+++ application/src/components/Login.tsx
@@ -2,44 +2,69 @@ import { Button, TextField } from "@mui/material";
 import style from "../styles/Login.module.css";
 import LoginIcon from '@mui/icons-material/Login';
 
-const Login = () => (
-  <div className={style.container}>
-    <div className={style.content}>
-      <h1>Missions Login</h1>
-      <div className={style.input}>
-        <TextField
-          type="email"
-          name="title"
-          label="Email"
-          fullWidth
-          className={style.text}
-          variant="filled"
-          sx={{ backgroundColor: "white" }}
-          required
-        />
-      </div>
-      <div className={style.input}>
-        <TextField
-            type="password"
+type LoginProps = {
+  emailVal: string;
+  handleSetEmail: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  passwordVal: string;
+  handleSetPassword: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  handleClickButton: () => void;
+  errMsg: string | null;
+}
+
+const Login = (props: LoginProps) => {
+  const {
+    emailVal,
+    handleSetEmail,
+    passwordVal,
+    handleSetPassword,
+    handleClickButton,
+    errMsg,
+  } = props;
+
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <h1>Missions Login</h1>
+        <p>{errMsg}</p>
+        <div className={style.input}>
+          <TextField
+            type="email"
             name="title"
-            label="Password"
+            label="Email"
             fullWidth
             className={style.text}
             variant="filled"
             sx={{ backgroundColor: "white" }}
             required
+            value={emailVal}
+            onChange={handleSetEmail}
           />
-      </div>
-      <div className={style.input}>
-        <Button variant="contained">
-          <div className={style.icon}>
-            <LoginIcon />
-          </div>
-          Login
-        </Button>
+        </div>
+        <div className={style.input}>
+          <TextField
+              type="password"
+              name="title"
+              label="Password"
+              fullWidth
+              className={style.text}
+              variant="filled"
+              sx={{ backgroundColor: "white" }}
+              required
+              value={passwordVal}
+              onChange={handleSetPassword}
+            />
+        </div>
+        <div className={style.input}>
+          <Button variant="contained" onClick={handleClickButton}>
+            <div className={style.icon}>
+              <LoginIcon />
+            </div>
+            Login
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
