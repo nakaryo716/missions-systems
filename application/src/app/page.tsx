73965a74857b403ec4e3b4d8e1c7f1cf5f4d9feb
ryoNAKAYA <nakayaryo0716@gmail.com>
2025-02-02 22:01:47 +0900

    [add]Added get user info feature for verify auth

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,9 +1,35 @@
+'use client'
+import getUserInfoApi from "@/api/userInfoApi";
 import App from "@/components/App";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Root() {
-return(
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUserInfoApi().then(res => {
+      if (!res.ok) {
+        console.error(res.err);
+        router.push("/login");
+        return;
+      }
+      console.log(res.value);
+      setLoading(false);
+      return;
+    })
+  }, [router]);
+
+  return(
     <>
-      <App/>
+      {
+        loading ? 
+          <div style={{textAlign: "center", alignItems: "center"}}>
+            <h1>Now Loading...</h1>
+          </div>
+        : <App />
+      }
     </>
   )
 }
--- application/src/app/self/page.tsx
+++ application/src/app/self/page.tsx
@@ -1,7 +1,34 @@
+'use client'
+import getUserInfoApi from "@/api/userInfoApi";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function MyPage() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    getUserInfoApi().then(res => {
+      if (!res.ok) {
+        console.error(res.err);
+        router.push("/login");
+        return;
+      }
+      console.log(res.value);
+      setLoading(false);
+      return;
+    })
+  }, [router]);
+  
   return(
     <>
-      <h1>MyPage</h1>
+      {
+        loading ? 
+          <div style={{textAlign: "center", alignItems: "center"}}>
+            <h1>Now Loading...</h1>
+          </div>
+        : <h1>My Page</h1>
+      }
     </>
-  )
+  );
 }
